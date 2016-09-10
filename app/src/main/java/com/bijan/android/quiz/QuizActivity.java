d227cfd5b8c081990161bb2f7baffb42a2d70422
bijan102 <bfazeli1@gmail.com>
2016-09-10 12:12:26 -0700

    Added a questionBank array to hold nwq Question obj that will be displayed to the user everytime the user hits next. Also added a new referrence for the next button and a listener for when the user clicks on the next button.

--- app/src/main/java/com/bijan/android/quiz/QuizActivity.java
+++ app/src/main/java/com/bijan/android/quiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
@@ -11,13 +12,36 @@ public class QuizActivity extends AppCompatActivity {
     //The two buttons the user can click
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;     // A next button to go on to the next question
+
+    // A TextView for displaying a question
+    private TextView mQuestionTextView;
+
+    // Declare and initialize an array of Question objects, 'mQuestionBank', to serve as a question bank
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    // Declare and initialize a current index for the Question array to be 0, 'mCurrentIndex'
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        //Setting up listeners for onClick by user
+        // Obtaining reference for the TextView
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        //setting mQuestionTextView's text to the question at the current index
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+
+        //Setting up listeners for onClick events triggered by the user after obtaining references
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +59,16 @@ public class QuizActivity extends AppCompatActivity {
                 Toast.makeText(QuizActivity.this,
                                 R.string.correct_toast,
                                 Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                int question = mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);
             }
         });
     }
