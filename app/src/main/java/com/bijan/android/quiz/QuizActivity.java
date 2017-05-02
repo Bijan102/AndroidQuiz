6ed043e634f659ca2d689b1ff11e841902879aef
bijan102 <bfazeli1@gmail.com>
2017-05-02 10:31:24 -0700

    Update: Created a custom landscape layout for when the user switches the layout from portrait to landscape.

--- app/src/main/java/com/bijan/android/quiz/QuizActivity.java
+++ app/src/main/java/com/bijan/android/quiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bijan.android.quiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,9 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
     private ImageButton mNextButton;     // A next button to go on to the next question
     private ImageButton mPreviousButton;
+
+    // Create a TAG to use for debugging program
+    private static final String TAG = "QuizActivity";
 
     // A TextView for displaying a question
     private TextView mQuestionTextView;
@@ -59,9 +63,60 @@ public class QuizActivity extends AppCompatActivity {
         toast.show();
     }
 
+    /**
+     * Dispatch onStart() to all fragments.  Ensure any created loaders are
+     * now started.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    /**
+     * Dispatch onResume() to fragments.  Note that for better inter-operation
+     * with older versions of the platform, at the point of this call the
+     * fragments attached to the activity are <em>not</em> resumed.  This means
+     * that in some cases the previous state may still be saved, not allowing
+     * fragment transactions that modify the state.  To correctly interact
+     * with fragments in their proper state, you should instead override
+     * {@link #onResumeFragments()}.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    /**
+     * Dispatch onPause() to fragments.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "onCreate(Bundle) called");
+
+
+
         setContentView(R.layout.activity_quiz);
 
         // Obtaining reference for the TextView
